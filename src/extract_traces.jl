9d45c46eb6f17e6bf071728952d7f9f464106698
goldenratio1618 <adamatanas@gmail.com>
2021-12-03 18:37:30 -0500

    Made extract_roi_overlap function thread-safe

--- src/extract_traces.jl
+++ src/extract_traces.jl
@@ -234,45 +234,63 @@ the moving and fixed datasets have the same path dictionary.
 function extract_roi_overlap(best_reg::Dict, param_path::Dict, param::Dict; reg_dir_key::String="path_dir_reg",
         transformed_dir_key::String="path_dir_transformed", reg_problems_key::String="path_reg_prob",
         param_path_moving::Union{Dict,Nothing}=nothing)
-    roi_overlaps = Dict()
-    roi_activity_diff = Dict()
-    errors = Dict()
+
+    problems = load_registration_problems([param_path[reg_problems_key]])
+
+    roi_overlaps = Vector{Dict}(undef, length(problems))
+    roi_activity_diff = Vector{Dict}(undef, length(problems))
+    errors = Vector{Exception}(undef, length(problems))
 
     if isnothing(param_path_moving)
         param_path_moving = param_path
     end
 
-    problems = load_registration_problems([param_path[reg_problems_key]])
+    @sync begin
+        Threads.@threads for i in 1:length(problems)
+            (moving, fixed) = problems[i]
+            try
+                dir = "$(moving)to$(fixed)"
+                # Bspline registration failed
+                if best_reg[(moving, fixed)][1] == 0
+                    continue
+                end
+                best = best_reg[(moving, fixed)]
+                tf_base = joinpath(param_path[reg_dir_key], "$(dir)/TransformParameters.$(best[1]).R$(best[2])")
+                img, result = run_transformix_roi(joinpath(param_path[reg_dir_key], "$(dir)"), 
+                    joinpath(param_path_moving["path_dir_roi_watershed"], "$(moving).mhd"),  joinpath(param_path[transformed_dir_key], "$(dir)"), 
+                    "$(tf_base).txt", "$(tf_base)_roi.txt", param_path["path_transformix"])
+                roi = read_img(MHD(joinpath(param_path["path_dir_roi_watershed"], "$(fixed).mhd")))
+                roi_regmap = delete_smeared_neurons(img, param["smeared_neuron_threshold"])
 
-    Threads.@threads for (moving, fixed) in problems
-        try
-            dir = "$(moving)to$(fixed)"
-            # Bspline registration failed
-            if best_reg[(moving, fixed)][1] == 0
-                continue
+                roi_overlap, roi_activity = register_neurons_overlap(roi_regmap, roi, 
+                    read_activity(joinpath(param_path_moving["path_dir_marker_signal"], "$(moving).txt")), 
+                    read_activity(joinpath(param_path["path_dir_marker_signal"], "$(fixed).txt")))
+                roi_overlaps[i] = roi_overlap
+                roi_activity_diff[i] = roi_activity
+            catch e
+                errors[i] = e
             end
-            best = best_reg[(moving, fixed)]
-            tf_base = joinpath(param_path[reg_dir_key], "$(dir)/TransformParameters.$(best[1]).R$(best[2])")
-            img, result = run_transformix_roi(joinpath(param_path[reg_dir_key], "$(dir)"), 
-                joinpath(param_path_moving["path_dir_roi_watershed"], "$(moving).mhd"),  joinpath(param_path[transformed_dir_key], "$(dir)"), 
-                "$(tf_base).txt", "$(tf_base)_roi.txt", param_path["path_transformix"])
-            roi = read_img(MHD(joinpath(param_path["path_dir_roi_watershed"], "$(fixed).mhd")))
-            roi_regmap = delete_smeared_neurons(img, param["smeared_neuron_threshold"])
-
-            roi_overlap, roi_activity = register_neurons_overlap(roi_regmap, roi, 
-                read_activity(joinpath(param_path_moving["path_dir_marker_signal"], "$(moving).txt")), 
-                read_activity(joinpath(param_path["path_dir_marker_signal"], "$(fixed).txt")))
-            roi_overlaps[(moving, fixed)] = roi_overlap
-            roi_activity_diff[(moving, fixed)] = roi_activity
-        catch e
-            errors[(moving, fixed)] = e
         end
     end
 
-    if length(keys(errors)) > 0
+    roi_overlaps_dict = Dict()
+    roi_activity_diff_dict = Dict()
+    errors_dict = Dict()
+    for (i, problem) in enumerate(problems)
+        if isassigned(roi_overlaps, i)
+            roi_overlaps_dict[problem] = roi_overlaps[i]
+        end
+        if isassigned(roi_activity_diff, i)
+            roi_activity_diff_dict[problem] = roi_activity_diff[i]
+        end
+        if isassigned(errors, i)
+            errors_dict[problem] = errors[i]
+        end
+    end
+    if length(keys(errors_dict)) > 0
         @warn "Registration issues at some time points"
     end
-    return roi_overlaps, roi_activity_diff, errors
+    return roi_overlaps_dict, roi_activity_diff_dict, errors_dict
 end
 
 
